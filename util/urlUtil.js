76cda31c393005b9d0ec4b54c17120a30dddfcec
dirtandgrass <verdant.luke@gmail.com>
2024-02-26 14:04:45 -0500

    add isValid utility f(x), rename utility module file

---
+++ util/urlUtil.js
@@ -0,0 +1,33 @@
+
+/**
+ * Generates a random string of ascii alpha-numeric characters
+ * @param {number} number of characters to generate
+ * @returns
+ */
+const generateRandomString = (length = 6) => {
+  if (typeof length !== 'number' || length < 1) throw new Error('length must be a number');
+
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+/**
+ * Validates a url
+ * @param {*} url the url to validate
+ * @returns {boolean} true if the url is valid, false otherwise
+ */
+const isValidUrl = (url) => {
+  try {
+    new URL(url);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+module.exports = {generateRandomString, isValidUrl};
