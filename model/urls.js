1c9559b3a7732d86d79be4b678cbe0034801534d
dirtandgrass <verdant.luke@gmail.com>
2024-03-04 12:30:59 -0500

    add server script start time as dateCreated for shortUrl objects

--- model/urls.js
+++ model/urls.js
@@ -2,14 +2,16 @@ const urlDatabase = {
   b6UTxQ: {
     longURL: "https://www.tsn.ca",
     userId: "userRandomID",
+    dateCreated: new Date(),
   },
   i3BoGr: {
     longURL: "https://www.google.ca",
     userId: "user2RandomID",
+    dateCreated: new Date(),
   },
 };
 /**
- * Retrieves all short urls for a given user id
+ * Retrieves all short url objects for a given user id
  * @param {string} id
  * @returns an array of short url objects
  */
